5be64804175dcc5a7b69ac59e8d0ebd47989c756
cmlnodzak <cnodzak@uncc.edu>
2016-11-11 17:31:17 -0500

    changed var

--- misc_tools.py
+++ misc_tools.py
@@ -55,8 +55,8 @@ def anagramCountCheck(s1,s2):
     for i in range(len(s1)):
         pos = ord(s1[i])-ord('a')
         c1[pos] = c1[pos] + 1
-    for i in range(len(s2)):
-        pos = ord(s2[i])-ord('a')
+    for g in range(len(s2)):
+        pos = ord(s2[g])-ord('a')
         c2[pos] = c2[pos] + 1
 
     j = 0
