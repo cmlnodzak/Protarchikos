6fb18a5039f4812c2d8d506ee316571a898f654c
Mitch Nodzak <mitchnodzak@mncomputer.local>
2016-11-11 17:01:54 -0500

    made miscellanous tools for teaching

---
+++ misc_tools.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# A collection of functions created while practicing.
+
+# anagramCheckCheck will check if two strings are in fact anagrams of one another.
+# using an inefficient method of checking each letter.
+
+def anagramCheckCheck(s1,s2):
+    alist = list(s2)
+    pos1 = 0 
+    stillOK = True
+
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+        pos1 = pos1 + 1
+    
+    return stillOK
+
+## Now a sort and compare method to increase efficiency.
+
+def anagramSortCheck(s1, s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+
+    alist1.sort()
+    alist2.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if alist1[pos] == alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+
+    return matches
+
+## Finally, a method of counting each letter. Anagrams should have the same number of each letter by definition.
+
+def anagramCountCheck(s1,s2):
+    c1 = [0]*26
+    c2 = [0]*26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i])-ord('a')
+        c1[pos] = c1[pos] + 1
+    for i in range(len(s2)):
+        pos = ord(s2[i])-ord('a')
+        c2[pos] = c2[pos] + 1
+
+    j = 0
+    stillOK = True
+    while j < 26 and stillOK:
+        if c1[j] == c2[j]:
+            j = j + 1
+        else:
+            stillOK = False
+    
+    return stillOK
