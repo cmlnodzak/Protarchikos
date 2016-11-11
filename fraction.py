581333c8f1ec09638d34e86acc1160a8cdad4488
Mitch Nodzak <mitchnodzak@mncomputer.local>
2016-11-11 16:10:06 -0500

    added sqrt func

--- fraction.py
+++ fraction.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+# This module includes math functions and defines a class to handle functions.
 
 
+# This function employs Newton's Method for estimation of square roots.
+def squareroot(n):
+    root = n/2
+    for k in range(20):
+        root = (1/2)*(root + (n / root))
+    return root
+
+# This function utilizes Euler's algorithm to calculate the GCD between
+# two numbers.
 def greatcommdiv(m,n,):
     while m % n != 0:
         oldm = m
