f0e80eb80f9a69d628ba09ae569f9ad1f4725e04
cmlnodzak <cnodzak@uncc.edu>
2016-11-14 12:22:05 -0500

    queued up

--- Structures.py
+++ Structures.py
@@ -38,3 +38,18 @@ class slowStack:
     def size(self):
         return len(self.items)
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return sef.items.pop()
+
+    def size(self):
+        return len(self.items)
